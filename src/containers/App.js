317c08a2da226df2455fc7016628ef997dd39fe7
lqhoangduy <lqhoangduy@gmail.com>
2022-11-30 23:59:39 +0700

    chore: remove custom scroll

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,6 @@ import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter as Router } from "connected-react-router";
 import { history } from "../redux";
-import { ToastContainer } from "react-toastify";
-import CustomScrollbars from "./../components/CustomScrollbars";
 import HomePage from "../containers/HomePage/HomePage";
 import NotFound from "../components/NotFound/NotFound";
 
@@ -20,7 +18,6 @@ import Login from "./Auth/Login";
 import System from "../routes/System";
 import AdminLayout from "./AdminLayout/AdminLayout";
 
-import { CustomToastCloseButton } from "../components/CustomToast";
 import DetailDoctor from "./Patient/Doctor/DetailDoctor";
 import VerifyBooking from "../components/VerifyBooking/VerifyBooking";
 import DetailSpecialty from "./Patient/Specialty/DetailSpecialty";
@@ -61,53 +58,35 @@ class App extends Component {
 					<div className='main-container'>
 						<MapLayout isLoggedIn={this.props.isLoggedIn}>
 							<div className='content-container'>
-								<CustomScrollbars style={{ height: "100vh", width: "100%" }}>
-									<Switch>
-										<Route path={path.HOME} exact component={Home} />
-										<Route
-											path={path.LOGIN}
-											component={userIsNotAuthenticated(Login)}
-										/>
-										<Route
-											path={path.SYSTEM}
-											component={userIsAuthenticated(System)}
-										/>
-										<Route path={path.HOMEPAGE} component={HomePage} />
-										<Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
-										<Route path={path.DOCTOR} component={ListDoctor} />
-										<Route
-											path={path.DETAIL_SPECIALTY}
-											component={DetailSpecialty}
-										/>
-										<Route path={path.SPECIALTY} component={ListSpecialty} />
-										<Route path={path.DETAIL_CLINIC} component={DetailClinic} />
-										<Route path={path.CLINIC} component={ListClinic} />
-										<Route
-											path={path.HANDBOOK_DETAIL}
-											component={DetailHandbook}
-										/>
-										<Route path={path.HANDBOOK} component={ListHandbook} />
-										<Route
-											path={path.VERIFY_BOOKING}
-											component={VerifyBooking}
-										/>
-										<Route path='*' component={NotFound} />
-									</Switch>
-								</CustomScrollbars>
+								<Switch>
+									<Route path={path.HOME} exact component={Home} />
+									<Route
+										path={path.LOGIN}
+										component={userIsNotAuthenticated(Login)}
+									/>
+									<Route
+										path={path.SYSTEM}
+										component={userIsAuthenticated(System)}
+									/>
+									<Route path={path.HOMEPAGE} component={HomePage} />
+									<Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
+									<Route path={path.DOCTOR} component={ListDoctor} />
+									<Route
+										path={path.DETAIL_SPECIALTY}
+										component={DetailSpecialty}
+									/>
+									<Route path={path.SPECIALTY} component={ListSpecialty} />
+									<Route path={path.DETAIL_CLINIC} component={DetailClinic} />
+									<Route path={path.CLINIC} component={ListClinic} />
+									<Route
+										path={path.HANDBOOK_DETAIL}
+										component={DetailHandbook}
+									/>
+									<Route path={path.HANDBOOK} component={ListHandbook} />
+									<Route path={path.VERIFY_BOOKING} component={VerifyBooking} />
+									<Route path='*' component={NotFound} />
+								</Switch>
 							</div>
-
-							<ToastContainer
-								className='toast-container'
-								toastClassName='toast-item'
-								bodyClassName='toast-item-body'
-								autoClose={false}
-								hideProgressBar={true}
-								pauseOnHover={false}
-								pauseOnFocusLoss={true}
-								closeOnClick={false}
-								draggable={false}
-								closeButton={<CustomToastCloseButton />}
-							/>
 						</MapLayout>
 					</div>
 				</Router>
